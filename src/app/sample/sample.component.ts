8460e783350d5f0438095802842fe7f4675e327b
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2022-01-18 15:02:36 +0900

    サンプルコンポーネントを追加。

---
+++ src/app/sample/sample.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * サンプルコンポーネント。
+ */
+@Component({
+  selector: 'app-sample',
+  templateUrl: './sample.component.html',
+  styleUrls: ['./sample.component.css']
+})
+export class SampleComponent implements OnInit {
+
+  public constructor() { }
+
+  public ngOnInit(): void {
+  }
+
+}
